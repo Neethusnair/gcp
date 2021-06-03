f36d19fc7b90bb40e9c7c49c5e597d6477dfd23f
Neethu <neethu.somasekharan@sreyo.com>
2021-06-03 08:53:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,15 +15,34 @@ resource "google_compute_instance" "vm_instance" {
     }
   }
 
-  network_interface {
-    # A default network is created for all GCP projects
-    network = google_compute_network.vpc_network.self_link
-    access_config {
-    }
+#   network_interface {
+#     # A default network is created for all GCP projects
+#     network = google_compute_network.vpc_network.self_link
+#     access_config {
+#     }
+#   }
+}
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.default.name
+
+  allow {
+    protocol = "icmp"
   }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000"]
+  }
+
+  source_tags = ["web"]
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "terraform-network"
-  auto_create_subnetworks = "true"
+resource "google_compute_network" "default" {
+  name = "test-network"
 }
+
+# resource "google_compute_network" "vpc_network" {
+#   name                    = "terraform-network"
+#   auto_create_subnetworks = "true"
+# }
