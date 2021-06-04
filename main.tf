3dbd5088eabe8277bbec29472935a9f0019f6dd6
Neethu <neethu.somasekharan@sreyo.com>
2021-06-03 18:11:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,8 @@ resource "google_compute_instance" "vm_instance" {
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-9"
+#       image = "debian-cloud/debian-9"
+      image = "debian-cloud/debian-10"
     }
   }
 
